fb7a5f10417a40c26a77ca2dbfd0a5bdec99894c
Vetrivelu Murugesan <75613547+vetrivelu@users.noreply.github.com>
2022-09-16 21:20:27 +0530

    first commit

---
+++ lib/pages/feeds.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:social_media_app/chats/recent_chats.dart';
+import 'package:social_media_app/models/post.dart';
+import 'package:social_media_app/utils/constants.dart';
+import 'package:social_media_app/utils/firebase.dart';
+import 'package:social_media_app/widgets/userpost.dart';
+
+class Feeds extends StatefulWidget {
+  @override
+  _FeedsState createState() => _FeedsState();
+}
+
+class _FeedsState extends State<Feeds> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int page = 5;
+  bool loadingMore = false;
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
+        setState(() {
+          page = page + 5;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          Constants.appName,
+          style: TextStyle(fontWeight: FontWeight.w900),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              CupertinoIcons.chat_bubble_2_fill,
+              size: 30.0,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (_) => Chats(),
+                ),
+              );
+            },
+          ),
+          SizedBox(width: 20.0),
+        ],
+      ),
+      body: FutureBuilder(
+        future: postRef.orderBy('timestamp', descending: true).limit(page).get(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            var snap = snapshot.data;
+            List docs = snap!.docs;
+            return ListView.builder(
+              controller: scrollController,
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                PostModel posts = PostModel.fromJson(docs[index].data());
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: UserPost(post: posts),
+                );
+              },
+            );
+          } else
+            return Center(
+              child: Text('No Feeds'),
+            );
+        },
+      ),
+    );
+  }
+}
